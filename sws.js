3bc42c7ea4a3a42a4a1cacc21400db05bf120cc3
Michael <stoltenta@post.com>
2026-06-12 10:09:01 +0000

    Update sws.js

--- sws.js
+++ sws.js
@@ -1,33 +1,18 @@
-const CACHE_NAME = 'dynamicka-pwa-cache-v1';
+// This is the "Offline copy of pages" service worker
 
-// Pri prvom načítaní uložíme len absolútny základ aplikácie
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['index.html', 'manifest.json']);
-    })
-  );
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// Automaticky zachytí a uloží do pamäte každé CSS, JS aj obrázok, ktorý web použije
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      // Ak už súbor máme v pamäti, okamžite ho načítame (rýchly štart / offline)
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      // Ak ho nemáme, stiahneme ho z webu a rovno odložíme do pamäte na neskôr
-      return fetch(e.request).then((networkResponse) => {
-        if (networkResponse && networkResponse.status === 200 && e.request.url.startsWith(self.location.origin)) {
-          const responseToCache = networkResponse.clone();
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(e.request, responseToCache);
-          });
-        }
-        return networkResponse;
-      });
-    })
-  );
-});
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
